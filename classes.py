085d0aca5d6e00cbaab7bb44123ffc9c752d228b
bradentl <braden.lee.nyb@gmail.com>
2022-11-16 02:17:34 -0500

    Added assets, completed basic rotation and player firing

--- classes.py
+++ classes.py
@@ -1,7 +1,9 @@
+import math
+
 class Board:
 	def __init__(self, dim):
-		self.dim = dim
-		self.player = Player((dim[0] / 2, dim[1] / 2))
+		self.dim = dim[0] / 10
+		self.player = Player((dim[0] / 2, (4 * dim[1]) / 5))
 		self.cores = set()
 		self.blocks = set()
 
@@ -12,7 +14,8 @@ class Player:
 		self.pos = pos
 		self.rot = 0
 		self.health = 3
-		self.projectiles = set()
+		self.projs = set()
+		self.timeDelay = 0
 
 	def move(self, x1, y1):
 		x0, y0 = self.pos[0], self.pos[1]
@@ -24,6 +27,32 @@ class Player:
 		if(self.rot < 0):
 			self.rot = 360 + self.rot
 
+	def createProjectile(self, l):
+		if(self.timeDelay > 0):
+			return
+
+		if(self.rot % 180 == 0):
+			m = (-l, 0) if (self.rot == 0) else (l, 0)
+		else:
+			# Modifies angle starting position to reflect typical unit circle
+			rot = (90 + self.rot) % 360
+			# Uses Law of Sines to calculate vector components
+			vx = math.sin(math.radians(90 - rot)) * l
+			vy = math.sin(math.radians(rot)) * l
+			# Determine positive/negative direction depending on quadrant
+			pass
+			# Slope of projectile vector, represented as rise-run tuple
+			# vy is negative since canvas y-direction is inverted from standard Cartesian plane
+			m = (-vy, vx) 
+		pos = self.pos # placeholder value for debug
+		proj = PlayerProjectile(pos, m, self.rot)
+		self.projs.add(proj)
+		self.timeDelay = 2
+
+	def deductHealth(self):
+		self.health -= 1
+		return (self.health > 0)
+
 	def area():
 		# top triangle: 0.7/1.0
 		# bottom triangle: 0.3/1.0
@@ -32,48 +61,55 @@ class Player:
 	def detectCollision(self, pos):
 		pass
 
-	def deductHealth(self):
-		self.health -= 1
-		return (self.health > 0)
-
 class Projectile:
-	def __init__(self, pos, vel):
-		pass
+	def __init__(self, pos, m, rot):
+		self.pos = pos
+		self.m = m
+		self.rot = rot
+		self.lifespan = 10
+
+	def move(self):
+		x0, y0 = self.pos[0], self.pos[1]
+		self.pos = (x0 + self.m[1], y0 + self.m[0])
 
 class PlayerProjectile(Projectile):
-	imgPath = ""
+	imgPath = "assets/playerProjectile.png"
 
-	def __init__(self, pos, vel):
-		pass
+	def __init__(self, pos, m, rot):
+		super().__init__(pos, m, rot)
 
 class OrangeProjectile(Projectile):
-	imgPath = ""
+	imgPath = "assets/oProjectile.png"
 
-	def __init__(self):
+	def __init__(self, pos, m, rot):
+		super().__init__(pos, m, rot)
 		self.health = 1
 
-class BlackProjectile(Projectile):
-	pass
+class PurpleProjectile(Projectile):
+	imgPath = "assets/pProjectile.png"
 
-class EnemyCore:
-	imgPath = ""
+	def __init__(self, pos, m, rot):
+		super().__init__(pos, m, rot)
+
+class Core:
+	imgPath = "assets/core.png"
 
 	def __init__(self, shield):
 		self.health = 5
 		self.shield = shield
 
 class Block:
-	imgPath = ""
+	imgPath = "assets/block.png"
 
 	def __init__(self):
 		pass
 
 class DestructableBlock(Block):
-	imgPath = ""
+	imgPath = "assets/dBlock.png"
 
 	def __init__(self):
 		super().__init__()
 		self.health = 5 # arbitrary number
 
 class EnemyBlock(Block):
-	imgPath = ""
+	imgPath = "assets/eBlock.png"
--- main.py
+++ main.py
@@ -3,25 +3,41 @@ from cmu_112_graphics import *
 
 import classes as c
 
+############
 res = (500, 500)
 
-############
 # This function hardcodes for simpler debugging
 def debug(app):
-	margin = app.width / 10
-	app.board = c.Board((app.width - margin, app.width - margin))
+	scale = 1/3
+	app.board = c.Board((app.width, app.height))
+	app.playerImg = app.scaleImage(app.loadImage(app.board.player.imgPath), scale)
+	app.projImg = app.scaleImage(app.loadImage("assets/playerProjectile.png"), scale)
 ############
 
 def appStarted(app):
-	# app.gameActive = False
+	app.board = None
 	debug(app) # Remember to remove in final product
-	app.image = app.loadImage(app.board.player.imgPath)
 
 def timerFired(app):
-    pass
+	if(not app.board):
+		return
+
+	if(app.board.player.timeDelay > 0):
+		app.board.player.timeDelay -= 1
+
+	# Iterates over a copied set to allow removal of elements
+	for proj in app.board.player.projs.copy():
+		if(proj.lifespan <= 0):
+			# Is a __hash__ method necessary?
+			app.board.player.projs.remove(proj)
+		else:
+			print(f'{len(app.board.player.projs)}: {proj}')
+			proj.move()
+			proj.lifespan -= 1
 
 def keyPressed(app, event):
-	if(not app.board): # board is active (FIX LATER)
+	# If board is inactive, skip board-related commands
+	if(not app.board):
 		return
 
 	match event.key:
@@ -29,15 +45,39 @@ def keyPressed(app, event):
 		case 'Down':	app.board.player.move(0, +5)
 		case "Left":	app.board.player.move(-5, 0)
 		case "Right":	app.board.player.move(+5, 0)
-		case "r": 		app.board.player.rotate(10)
-		case "Space":
-			# fire projectiles
-			pass
+		case "r": 		app.board.player.rotate(-10)
+		case "e": 		app.board.player.rotate(+10)
+		case "Space":	app.board.player.createProjectile(app.playerImg.height)
 
 def redrawAll(app, canvas):
+	# Drawing background
 	canvas.create_rectangle(0, 0, app.width, app.height, fill="gray")
-	canvas.create_image(app.board.player.pos[0], app.board.player.pos[1],
-						image=ImageTk.PhotoImage(app.scaleImage(app.image.rotate(app.board.player.rot), 1/3)))
+	if(app.board):
+		# Drawing board
+		canvas.create_rectangle(app.board.dim, app.board.dim, app.width - app.board.dim, app.height - app.board.dim,
+								fill="#c4bea6", outline="#c4bea6")
+		# Drawing player
+		canvas.create_image(app.board.player.pos[0], app.board.player.pos[1],
+							image=ImageTk.PhotoImage(app.playerImg.rotate(app.board.player.rot)))
+		
+		# Drawing projectiles
+		for proj in app.board.player.projs:
+			canvas.create_image(proj.pos[0], proj.pos[1],
+							image=ImageTk.PhotoImage(app.projImg.rotate(proj.rot)))
+
+		# Debug visualizations
+		canvas.create_line(app.width / 2, 0, app.width / 2, app.height, fill="red")
+		canvas.create_line(0, app.height / 2, app.width, app.height / 2, fill="red")
+		cx, cy = app.board.player.pos[0], app.board.player.pos[1]
+		canvas.create_oval(cx-5, cy-5, cx+5, cy+5, fill="red")
+		x1, y1 = app.board.player.pos[0] - (app.playerImg.width / 2), app.board.player.pos[1] + (app.playerImg.height / 2)
+		x2, y2 = app.board.player.pos[0] + (app.playerImg.width / 2), app.board.player.pos[1] - (app.playerImg.height / 2)
+		canvas.create_rectangle(x1, y1, x2, y2, outline="black")
+		cx, cy = app.board.player.pos[0], app.board.player.pos[1]
+		r = app.playerImg.height / 2
+		canvas.create_oval(cx-r, cy-r, cx+r, cy+r, outline="black")
+		cx, cy = cx - (app.playerImg.width / 2), cy + (app.playerImg.height / 2)
+		canvas.create_oval(cx-5, cy-5, cx+5, cy+5, fill="red")
 
 def playGame():
 	runApp(width=res[0], height=res[1])
