4ddf04b48f12586cb377fa4f8147f7bba32d4ba1
bradentl <braden.lee.nyb@gmail.com>
2022-11-18 00:02:54 -0500

    Completed enemy orientation method

--- classes.py
+++ classes.py
@@ -1,11 +1,29 @@
 import math
 
 class Board:
-	def __init__(self, dim):
-		self.dim = dim[0] / 10
+	def __init__(self, dim, gridL=50):
+		# Since x=y, tuple contains x0 and x1 (or, y0 and y1)
+		self.dim = self.gameDimensions(dim[0])
+		# Is this correct grid implementation?
+		self.grid = [[None] * gridL for i in range(gridL)]
 		self.player = Player((dim[0] / 2, (4 * dim[1]) / 5))
-		self.cores = set()
-		self.blocks = set()
+		self.cores = self.initCores()
+		self.obstacles = set()
+		# Entities that can be destroyed
+		self.enemies = set()
+	
+	def gameDimensions(self, l):
+		margin = l / 10
+		return (margin, l - margin)
+	
+	def determineGrid(self):
+		pass
+
+	def initCores(self):
+		return set()
+
+	def gameWon(self):
+		return (len(self.cores) == 0)
 
 class Player:
 	imgPath = "assets/player.png"
@@ -20,7 +38,8 @@ class Player:
 	def move(self, x1, y1):
 		x0, y0 = self.pos[0], self.pos[1]
 		self.pos = (x0 + x1, y0 + y1)
-		print(self.pos)
+		# Returns original position in case movement must be undone
+		return (x0, y0)
 	
 	def rotate(self, d1):
 		self.rot = (self.rot + d1) % 360
@@ -31,22 +50,20 @@ class Player:
 		if(self.timeDelay > 0):
 			return
 
-		if(self.rot % 180 == 0):
-			m = (-l, 0) if (self.rot == 0) else (l, 0)
-		else:
-			# Modifies angle starting position to reflect typical unit circle
-			rot = (90 + self.rot) % 360
-			# Uses Law of Sines to calculate vector components
-			vx = math.sin(math.radians(90 - rot)) * l
-			vy = math.sin(math.radians(rot)) * l
-			# Determine positive/negative direction depending on quadrant
-			pass
-			# Slope of projectile vector, represented as rise-run tuple
-			# vy is negative since canvas y-direction is inverted from standard Cartesian plane
-			m = (-vy, vx) 
+		# Modifies angle starting position to reflect typical unit circle
+		rot = (90 + self.rot) % 360
+		# Uses Law of Sines to calculate vector components
+		vx = math.sin(math.radians(90 - rot)) * l
+		vy = math.sin(math.radians(rot)) * l
+		# Slope of projectile vector, represented as rise-run tuple
+		# vy is negative since canvas y-direction is inverted from standard Cartesian plane
+		m = (-vy, vx) 
+
+		# Determines position of origin (should be player tip)
 		pos = self.pos # placeholder value for debug
 		proj = PlayerProjectile(pos, m, self.rot)
 		self.projs.add(proj)
+		# timerFired() delay before projectile can be created
 		self.timeDelay = 2
 
 	def deductHealth(self):
@@ -94,9 +111,27 @@ class PurpleProjectile(Projectile):
 class Core:
 	imgPath = "assets/core.png"
 
-	def __init__(self, shield):
+	def __init__(self, pos):
+		self.pos = pos
 		self.health = 5
-		self.shield = shield
+	
+	def evade(self, pos=None):
+		if(not pos):
+			pos = self.pos
+		# recursive function finding furthest single legal move away from block
+		# runs each "turn" until is safely set distance apart
+		pass
+
+class ShieldedCore(Core):
+	imgPath = "assets/sCore.png"
+
+	def __init__(self, pos):
+		super().__init__(pos)
+		self.shield = True
+
+	def destroyShield(self):
+		self.shield = False
+		imgPath = super().imgPath
 
 class Block:
 	imgPath = "assets/block.png"
@@ -113,3 +148,29 @@ class DestructableBlock(Block):
 
 class EnemyBlock(Block):
 	imgPath = "assets/eBlock.png"
+
+class Enemy:
+	imgPath = "assets/enemy.png"
+
+	def __init__(self, pos):
+		self.pos = pos
+		self.rot = 0
+		# Is a variable necessary? They are defeated after a single projectile collision.
+		self.health = 1
+		self.projs = set()
+
+	# Enemy should always face player
+	def orientation(self, pos):
+		x0, y0 = self.pos[0], self.pos[1] # Enemy
+		x1, y1 = pos[0], pos[1] # Player
+		# Determine rotation angle from line drawn between enemy and player position
+		a = x1 - x0
+		b = y0 - y1 # y0 and y1 swapped since downwards is positive
+		c = math.sqrt(a**2 + b**2)
+		# Prevents division by zero
+		if(a == 0):
+			self.rot = 180 if (b < 0) else 0
+			return
+		# Law of Cosines angle calculation
+		rot = math.degrees(math.acos((a**2 + c**2 - b**2) / (2 * a * c)))
+		self.rot = ((-1 * (rot + 90)) if (b < 0) else (rot - 90)) % 360
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from classes import *
 from cmu_112_graphics import *
 
-import classes as c
+import random
+import classes
 
 ############
 res = (500, 500)
@@ -9,13 +10,28 @@ res = (500, 500)
 # This function hardcodes for simpler debugging
 def debug(app):
 	scale = 1/3
-	app.board = c.Board((app.width, app.height))
+	app.board = Board((app.width, app.height))
 	app.playerImg = app.scaleImage(app.loadImage(app.board.player.imgPath), scale)
 	app.projImg = app.scaleImage(app.loadImage("assets/playerProjectile.png"), scale)
+	app.coreImg = app.scaleImage(app.loadImage("assets/sCore.png"), scale)
+	app.enemyImg = app.scaleImage(app.loadImage("assets/enemy.png"), scale)
+
+def createCore(app):
+	pos = (random.randint(app.board.dim[0], app.board.dim[1]),
+		   random.randint(app.board.dim[0], app.board.dim[1]))
+	core = ShieldedCore(pos)
+	app.board.cores.add(core)
+
+def createEnemy(app):
+	pos = (random.randint(app.board.dim[0], app.board.dim[1]),
+		   random.randint(app.board.dim[0], app.board.dim[1]))
+	core = Enemy(pos)
+	app.board.enemies.add(core)
 ############
 
 def appStarted(app):
 	app.board = None
+	app.gameOver = False
 	debug(app) # Remember to remove in final product
 
 def timerFired(app):
@@ -25,13 +41,15 @@ def timerFired(app):
 	if(app.board.player.timeDelay > 0):
 		app.board.player.timeDelay -= 1
 
+	for enemy in app.board.enemies:
+		enemy.orientation(app.board.player.pos)
+
 	# Iterates over a copied set to allow removal of elements
 	for proj in app.board.player.projs.copy():
 		if(proj.lifespan <= 0):
 			# Is a __hash__ method necessary?
 			app.board.player.projs.remove(proj)
 		else:
-			print(f'{len(app.board.player.projs)}: {proj}')
 			proj.move()
 			proj.lifespan -= 1
 
@@ -48,22 +66,32 @@ def keyPressed(app, event):
 		case "r": 		app.board.player.rotate(-10)
 		case "e": 		app.board.player.rotate(+10)
 		case "Space":	app.board.player.createProjectile(app.playerImg.height)
+		# Debug keys
+		case "k":		createEnemy(app)
 
 def redrawAll(app, canvas):
 	# Drawing background
 	canvas.create_rectangle(0, 0, app.width, app.height, fill="gray")
 	if(app.board):
 		# Drawing board
-		canvas.create_rectangle(app.board.dim, app.board.dim, app.width - app.board.dim, app.height - app.board.dim,
+		canvas.create_rectangle(app.board.dim[0], app.board.dim[0], app.board.dim[1], app.board.dim[1],
 								fill="#c4bea6", outline="#c4bea6")
 		# Drawing player
 		canvas.create_image(app.board.player.pos[0], app.board.player.pos[1],
 							image=ImageTk.PhotoImage(app.playerImg.rotate(app.board.player.rot)))
-		
 		# Drawing projectiles
 		for proj in app.board.player.projs:
 			canvas.create_image(proj.pos[0], proj.pos[1],
 							image=ImageTk.PhotoImage(app.projImg.rotate(proj.rot)))
+		
+		# Drawing cores
+		for core in app.board.cores:
+			canvas.create_image(core.pos[0], core.pos[1],
+								image=ImageTk.PhotoImage(app.coreImg))
+		# Drawing enemies
+		for enemy in app.board.enemies:
+			canvas.create_image(enemy.pos[0], enemy.pos[1],
+								image=ImageTk.PhotoImage(app.enemyImg.rotate(enemy.rot)))
 
 		# Debug visualizations
 		canvas.create_line(app.width / 2, 0, app.width / 2, app.height, fill="red")
@@ -78,10 +106,14 @@ def redrawAll(app, canvas):
 		canvas.create_oval(cx-r, cy-r, cx+r, cy+r, outline="black")
 		cx, cy = cx - (app.playerImg.width / 2), cy + (app.playerImg.height / 2)
 		canvas.create_oval(cx-5, cy-5, cx+5, cy+5, fill="red")
+		for enemy in app.board.enemies:
+			canvas.create_line(enemy.pos[0], enemy.pos[1], app.board.player.pos[0], app.board.player.pos[1], fill="blue")
+			canvas.create_line(enemy.pos[0], enemy.pos[1], enemy.pos[0], app.board.player.pos[1], fill="blue", dash=(5,1))
+			canvas.create_line(enemy.pos[0], app.board.player.pos[1], app.board.player.pos[0], app.board.player.pos[1], fill="blue", dash=(5,1))
+			canvas.create_text(enemy.pos[0] + app.enemyImg.width * 2, enemy.pos[1] + app.enemyImg.height / 2,
+							   text=enemy.rot, font="Arial 10 bold", fill="black")
 
 def playGame():
 	runApp(width=res[0], height=res[1])
 
 playGame()
-
-# app.gameActive = app.player.deductHealth()
