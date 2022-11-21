8b580d49a4715d7786bcc1e8f21e42e39c611754
bradentl <braden.lee.nyb@gmail.com>
2022-11-21 15:23:42 -0500

    Defined basic firing patterns, basic following code

--- classes.py
+++ classes.py
@@ -1,16 +1,17 @@
 import math
+import random
 
 class Board:
-	def __init__(self, dim, gridL=50):
+	def __init__(self, dim, sCoreDim, gridL=50):
 		# Since x=y, tuple contains x0 and x1 (or, y0 and y1)
 		self.dim = self.gameDimensions(dim[0])
 		# Is this correct grid implementation?
 		self.grid = [[None] * gridL for i in range(gridL)]
 		self.player = Player((dim[0] / 2, (4 * dim[1]) / 5))
-		self.cores = self.initCores()
+		self.cores = self.initCores(sCoreDim)
 		self.obstacles = set()
 		# Entities that can be destroyed
-		self.enemies = set()
+		self.enemies = self.initEnemies()
 	
 	def gameDimensions(self, l):
 		margin = l / 10
@@ -19,8 +20,19 @@ class Board:
 	def determineGrid(self):
 		pass
 
-	def initCores(self):
+	def initCores(self, dim):
+		x = self.dim[0] + (dim / 2)
+		core = ShieldedCore((x, x))
+		return {core}
+	
+	def initEnemies(self):
 		return set()
+
+	def isLegalMove(self, pos, dim=0):
+		if(pos[0] < (self.dim[0] + (dim[0] / 2)) or pos[1] < (self.dim[0] + (dim[1] / 2)) or 
+		   pos[0] > (self.dim[1] - (dim[0] / 2)) or pos[1] > (self.dim[1] - (dim[1] / 2))):
+			return False
+		return True
 
 	def gameWon(self):
 		return (len(self.cores) == 0)
@@ -33,7 +45,9 @@ class Player:
 		self.rot = 0
 		self.health = 3
 		self.projs = set()
-		self.timeDelay = 0
+		self.firingDelay = 0
+		# Implement targeting system
+		self.target = None
 
 	def move(self, x1, y1):
 		x0, y0 = self.pos[0], self.pos[1]
@@ -47,7 +61,7 @@ class Player:
 			self.rot = 360 + self.rot
 
 	def createProjectile(self, l):
-		if(self.timeDelay > 0):
+		if(self.firingDelay > 0):
 			return
 
 		# Modifies angle starting position to reflect typical unit circle
@@ -64,11 +78,39 @@ class Player:
 		proj = PlayerProjectile(pos, m, self.rot)
 		self.projs.add(proj)
 		# timerFired() delay before projectile can be created
-		self.timeDelay = 2
+		self.firingDelay = 2
 
 	def deductHealth(self):
 		self.health -= 1
 		return (self.health > 0)
+
+	def determineTarget(self, enemies=set(), cores=set()):
+		targets = list(cores if (len(enemies) == 0) else enemies)
+		# Toggles targeting
+		if(self.target or len(targets) == 0):
+			self.target = None
+			return
+
+		def distance(x1, y1):
+			return math.sqrt((self.pos[0] - x1)**2 + (self.pos[1] - y1)**2)
+		enemyDistance = [distance(e.pos[0], e.pos[1]) for e in targets]
+
+		self.target = targets[enemyDistance.index(min(enemyDistance))]
+	
+	def autoOrient(self):
+		x0, y0 = self.pos[0], self.pos[1]
+		x1, y1 = self.target.pos[0], self.target.pos[1]
+		# Determine rotation angle from line drawn between enemy and player position
+		a = x1 - x0
+		b = y0 - y1 # y0 and y1 swapped since downwards is positive
+		c = math.sqrt(a**2 + b**2)
+		# Prevents division by zero
+		if(a == 0):
+			self.rot = 180 if (b < 0) else 0
+			return
+		# Law of Cosines angle calculation
+		rot = math.degrees(math.acos((a**2 + c**2 - b**2) / (2 * a * c)))
+		self.rot = ((-1 * (rot + 90)) if (b < 0) else (rot - 90)) % 360
 
 	def area():
 		# top triangle: 0.7/1.0
@@ -79,11 +121,11 @@ class Player:
 		pass
 
 class Projectile:
-	def __init__(self, pos, m, rot):
+	def __init__(self, pos, m, rot=0):
 		self.pos = pos
 		self.m = m
 		self.rot = rot
-		self.lifespan = 10
+		self.lifespan = 15
 
 	def move(self):
 		x0, y0 = self.pos[0], self.pos[1]
@@ -98,15 +140,17 @@ class PlayerProjectile(Projectile):
 class OrangeProjectile(Projectile):
 	imgPath = "assets/oProjectile.png"
 
-	def __init__(self, pos, m, rot):
-		super().__init__(pos, m, rot)
+	def __init__(self, pos, m):
+		super().__init__(pos, m)
 		self.health = 1
+		self.lifespan = self.lifespan * 2
 
 class PurpleProjectile(Projectile):
 	imgPath = "assets/pProjectile.png"
 
-	def __init__(self, pos, m, rot):
-		super().__init__(pos, m, rot)
+	def __init__(self, pos, m):
+		super().__init__(pos, m)
+		self.lifespan = self.lifespan * 2
 
 class Core:
 	imgPath = "assets/core.png"
@@ -114,13 +158,64 @@ class Core:
 	def __init__(self, pos):
 		self.pos = pos
 		self.health = 5
+		self.projs = set()
+		self.firingDelay = 0
 	
 	def evade(self, pos=None):
 		if(not pos):
 			pos = self.pos
-		# recursive function finding furthest single legal move away from block
+		# backtracking function finding furthest single legal move away from block
 		# runs each "turn" until is safely set distance apart
 		pass
+
+	def createProjectile(self, deg, l, pType=True):
+		# Define slope from angle
+		rot = (90 + deg) % 360
+		vx = math.sin(math.radians(90 - rot)) * l
+		vy = math.sin(math.radians(rot)) * l
+		# Slope of projectile vector, represented as rise-run tuple
+		# vy is negative since canvas y-direction is inverted from standard Cartesian plane
+		self.m = (-vy, vx)
+		# Values here are reduced by factor to slow projectiles down from standard speed
+		m = (self.m[0] / 2.5, self.m[1] / 2.5)
+		# Determines which type of projectile to add
+		proj = OrangeProjectile(self.pos, m) if pType else PurpleProjectile(self.pos, m)
+		self.projs.add(proj)
+	
+	def angleToPlayer(self, pPos):
+		x0, y0 = self.pos[0], self.pos[1] # Enemy
+		x1, y1 = pPos[0], pPos[1] # Player
+		# Determine rotation angle from line drawn between enemy and player position
+		a = x1 - x0
+		b = y0 - y1 # y0 and y1 swapped since downwards is positive
+		c = math.sqrt(a**2 + b**2)
+		# Prevents division by zero
+		if(a == 0):
+			self.rot = 180 if (b < 0) else 0
+			return
+		# Law of Cosines angle calculation
+		rot = math.degrees(math.acos((a**2 + c**2 - b**2) / (2 * a * c)))
+		rot = ((-1 * (rot + 90)) if (b < 0) else (rot - 90)) % 360
+		return rot
+
+	def firingPattern(self, pattern, l, pPos=None, pType=True):
+		if(self.firingDelay > 0):
+			return
+
+		match pattern:
+			case 1:
+				pass
+			case 2:
+				deg = self.angleToPlayer(pPos)
+				self.createProjectile(deg - 20, l)
+				self.createProjectile(deg, l)
+				self.createProjectile(deg + 20, l)
+				self.firingDelay = 3
+			case _:
+				for deg in range(0, 360, 45):
+					self.createProjectile(deg, l, pType)
+					pType = not pType
+				self.firingDelay = 10
 
 class ShieldedCore(Core):
 	imgPath = "assets/sCore.png"
@@ -131,36 +226,55 @@ class ShieldedCore(Core):
 
 	def destroyShield(self):
 		self.shield = False
-		imgPath = super().imgPath
 
 class Block:
 	imgPath = "assets/block.png"
 
-	def __init__(self):
-		pass
+	def __init__(self, pos):
+		self.pos = pos
 
 class DestructableBlock(Block):
 	imgPath = "assets/dBlock.png"
 
-	def __init__(self):
-		super().__init__()
+	def __init__(self, pos):
+		super().__init__(pos)
 		self.health = 5 # arbitrary number
 
 class EnemyBlock(Block):
 	imgPath = "assets/eBlock.png"
+
+	def __init__(self, pos):
+		super().__init__(pos)
 
 class Enemy:
 	imgPath = "assets/enemy.png"
 
 	def __init__(self, pos):
 		self.pos = pos
-		self.rot = 0
+		self.rot = 180
 		# Is a variable necessary? They are defeated after a single projectile collision.
 		self.health = 1
+		self.m = (0, 0)
 		self.projs = set()
+		self.firingDelay = 0
+	
+	# Slowly move forwards according to orientation
+	def follow(self, pPos, l):
+		self.orient(pPos)
+		# Modifies angle starting position to reflect typical unit circle
+		rot = (90 + self.rot) % 360
+		# Uses Law of Sines to calculate vector components
+		vx = math.sin(math.radians(90 - rot)) * l
+		vy = math.sin(math.radians(rot)) * l
+		# Slope of projectile vector, represented as rise-run tuple
+		# vy is negative since canvas y-direction is inverted from standard Cartesian plane
+		self.m = (-vy, vx)
+		pos = self.pos
+		self.pos = (pos[0] + (self.m[1] / 20), pos[1] + (self.m[0] / 20))
+		return pos
 
-	# Enemy should always face player
-	def orientation(self, pos):
+	# Enemy should slowly turn to face player
+	def orient(self, pos):
 		x0, y0 = self.pos[0], self.pos[1] # Enemy
 		x1, y1 = pos[0], pos[1] # Player
 		# Determine rotation angle from line drawn between enemy and player position
@@ -173,4 +287,25 @@ class Enemy:
 			return
 		# Law of Cosines angle calculation
 		rot = math.degrees(math.acos((a**2 + c**2 - b**2) / (2 * a * c)))
-		self.rot = ((-1 * (rot + 90)) if (b < 0) else (rot - 90)) % 360
+		rot = ((-1 * (rot + 90)) if (b < 0) else (rot - 90)) % 360
+		print(self.rot - rot)
+		if((self.rot - rot) < 0):
+			self.rot += 5
+		else:
+			self.rot -= 5
+	
+	def createProjectile(self):
+		# Values here are reduced by factor to slow projectiles down from standard speed
+		m = (self.m[0] / 2.5, self.m[1] / 2.5)
+		# Determines which type of projectile to add
+		proj = OrangeProjectile(self.pos, m)
+		self.projs.add(proj)
+		self.firingDelay = 15
+	
+	def autoFire(self):
+		if(self.firingDelay > 0):
+			return
+		self.createProjectile()
+	
+	def firingPattern(self, patternType):
+		pass
--- main.py
+++ main.py
@@ -2,47 +2,74 @@ from classes import *
 from cmu_112_graphics import *
 
 import random
-import classes
 
 ############
 res = (500, 500)
 
 # This function hardcodes for simpler debugging
 def debug(app):
-	scale = 1/3
-	app.board = Board((app.width, app.height))
-	app.playerImg = app.scaleImage(app.loadImage(app.board.player.imgPath), scale)
-	app.projImg = app.scaleImage(app.loadImage("assets/playerProjectile.png"), scale)
-	app.coreImg = app.scaleImage(app.loadImage("assets/sCore.png"), scale)
-	app.enemyImg = app.scaleImage(app.loadImage("assets/enemy.png"), scale)
-
-def createCore(app):
-	pos = (random.randint(app.board.dim[0], app.board.dim[1]),
-		   random.randint(app.board.dim[0], app.board.dim[1]))
-	core = ShieldedCore(pos)
-	app.board.cores.add(core)
+	app.board = Board((app.width, app.height), app.sCoreImg.width)
 
 def createEnemy(app):
 	pos = (random.randint(app.board.dim[0], app.board.dim[1]),
 		   random.randint(app.board.dim[0], app.board.dim[1]))
-	core = Enemy(pos)
-	app.board.enemies.add(core)
+	enemy = Enemy(pos)
+	app.board.enemies.add(enemy)
 ############
 
 def appStarted(app):
 	app.board = None
 	app.gameOver = False
+	initSprites(app)
 	debug(app) # Remember to remove in final product
+
+def initSprites(app, scale=1/3):
+	app.playerImg = app.scaleImage(app.loadImage("assets/player.png"), scale)
+	app.projImg = app.scaleImage(app.loadImage("assets/playerProjectile.png"), scale)
+	app.coreImg = app.scaleImage(app.loadImage("assets/core.png"), scale)
+	app.sCoreImg = app.scaleImage(app.loadImage("assets/sCore.png"), scale)
+	app.enemyImg = app.scaleImage(app.loadImage("assets/enemy.png"), scale)
+	app.oProjImg = app.scaleImage(app.loadImage("assets/oProjectile.png"), scale)
+	app.pProjImg = app.scaleImage(app.loadImage("assets/pProjectile.png"), scale)
 
 def timerFired(app):
 	if(not app.board):
 		return
 
-	if(app.board.player.timeDelay > 0):
-		app.board.player.timeDelay -= 1
+	if(app.board.player.firingDelay > 0):
+		app.board.player.firingDelay -= 1
+
+	if(app.board.player.target):
+		app.board.player.autoOrient()
+	
+	for core in app.board.cores:
+		if(core.firingDelay > 0):
+			core.firingDelay -= 1
+		
+		core.firingPattern(3, app.coreImg.height, app.board.player.pos)
+
+		for proj in core.projs.copy():
+			if(proj.lifespan <= 0):
+				core.projs.remove(proj)
+			else:
+				proj.move()
+				proj.lifespan -= 1
 
 	for enemy in app.board.enemies:
-		enemy.orientation(app.board.player.pos)
+		if(enemy.firingDelay > 0):
+			enemy.firingDelay -= 1
+
+		pos = enemy.follow(app.board.player.pos, app.enemyImg.height)
+		if(not app.board.isLegalMove(enemy.pos, (app.enemyImg.width, app.enemyImg.height))):
+			enemy.pos = pos
+		enemy.autoFire()
+
+		for proj in enemy.projs.copy():
+			if(proj.lifespan <= 0):
+				enemy.projs.remove(proj)
+			else:
+				proj.move()
+				proj.lifespan -= 1
 
 	# Iterates over a copied set to allow removal of elements
 	for proj in app.board.player.projs.copy():
@@ -57,17 +84,26 @@ def keyPressed(app, event):
 	# If board is inactive, skip board-related commands
 	if(not app.board):
 		return
-
+	
 	match event.key:
-		case 'Up':		app.board.player.move(0, -5)
-		case 'Down':	app.board.player.move(0, +5)
-		case "Left":	app.board.player.move(-5, 0)
-		case "Right":	app.board.player.move(+5, 0)
 		case "r": 		app.board.player.rotate(-10)
 		case "e": 		app.board.player.rotate(+10)
 		case "Space":	app.board.player.createProjectile(app.playerImg.height)
+		case "t":		app.board.player.determineTarget(app.board.enemies, app.board.cores)
 		# Debug keys
 		case "k":		createEnemy(app)
+
+	# Keys that require legality checks
+	def playerMovement():
+		match event.key:
+			case 'Up':		return app.board.player.move(0, -5)
+			case 'Down': 	return app.board.player.move(0, +5)
+			case "Left":	return app.board.player.move(-5, 0)
+			case "Right":	return app.board.player.move(+5, 0)
+	
+	pos = playerMovement()
+	if(not app.board.isLegalMove(app.board.player.pos, (app.playerImg.width, app.playerImg.height))):
+		app.board.player.pos = pos
 
 def redrawAll(app, canvas):
 	# Drawing background
@@ -79,6 +115,10 @@ def redrawAll(app, canvas):
 		# Drawing player
 		canvas.create_image(app.board.player.pos[0], app.board.player.pos[1],
 							image=ImageTk.PhotoImage(app.playerImg.rotate(app.board.player.rot)))
+		if(app.board.player.target):
+			canvas.create_line(app.board.player.pos[0], app.board.player.pos[1],
+							   app.board.player.target.pos[0], app.board.player.target.pos[1], fill="blue", dash=(5,1))
+
 		# Drawing projectiles
 		for proj in app.board.player.projs:
 			canvas.create_image(proj.pos[0], proj.pos[1],
@@ -87,31 +127,39 @@ def redrawAll(app, canvas):
 		# Drawing cores
 		for core in app.board.cores:
 			canvas.create_image(core.pos[0], core.pos[1],
-								image=ImageTk.PhotoImage(app.coreImg))
+								image=ImageTk.PhotoImage(app.sCoreImg if core.shield else app.coreImg))
+			
+			for proj in core.projs:
+				canvas.create_image(proj.pos[0], proj.pos[1],
+									image=ImageTk.PhotoImage(app.oProjImg if (type(proj) == OrangeProjectile) else app.pProjImg))
+
 		# Drawing enemies
 		for enemy in app.board.enemies:
 			canvas.create_image(enemy.pos[0], enemy.pos[1],
 								image=ImageTk.PhotoImage(app.enemyImg.rotate(enemy.rot)))
+			for proj in enemy.projs:
+				canvas.create_image(proj.pos[0], proj.pos[1],
+									image=ImageTk.PhotoImage(app.oProjImg if (type(proj) == OrangeProjectile) else app.pProjImg))
 
 		# Debug visualizations
-		canvas.create_line(app.width / 2, 0, app.width / 2, app.height, fill="red")
-		canvas.create_line(0, app.height / 2, app.width, app.height / 2, fill="red")
+		# canvas.create_line(app.width / 2, 0, app.width / 2, app.height, fill="red")
+		# canvas.create_line(0, app.height / 2, app.width, app.height / 2, fill="red")
 		cx, cy = app.board.player.pos[0], app.board.player.pos[1]
-		canvas.create_oval(cx-5, cy-5, cx+5, cy+5, fill="red")
+		# canvas.create_oval(cx-5, cy-5, cx+5, cy+5, fill="red")
 		x1, y1 = app.board.player.pos[0] - (app.playerImg.width / 2), app.board.player.pos[1] + (app.playerImg.height / 2)
 		x2, y2 = app.board.player.pos[0] + (app.playerImg.width / 2), app.board.player.pos[1] - (app.playerImg.height / 2)
-		canvas.create_rectangle(x1, y1, x2, y2, outline="black")
+		# canvas.create_rectangle(x1, y1, x2, y2, outline="black")
 		cx, cy = app.board.player.pos[0], app.board.player.pos[1]
 		r = app.playerImg.height / 2
-		canvas.create_oval(cx-r, cy-r, cx+r, cy+r, outline="black")
+		# canvas.create_oval(cx-r, cy-r, cx+r, cy+r, outline="black")
 		cx, cy = cx - (app.playerImg.width / 2), cy + (app.playerImg.height / 2)
-		canvas.create_oval(cx-5, cy-5, cx+5, cy+5, fill="red")
-		for enemy in app.board.enemies:
-			canvas.create_line(enemy.pos[0], enemy.pos[1], app.board.player.pos[0], app.board.player.pos[1], fill="blue")
-			canvas.create_line(enemy.pos[0], enemy.pos[1], enemy.pos[0], app.board.player.pos[1], fill="blue", dash=(5,1))
-			canvas.create_line(enemy.pos[0], app.board.player.pos[1], app.board.player.pos[0], app.board.player.pos[1], fill="blue", dash=(5,1))
-			canvas.create_text(enemy.pos[0] + app.enemyImg.width * 2, enemy.pos[1] + app.enemyImg.height / 2,
-							   text=enemy.rot, font="Arial 10 bold", fill="black")
+		# canvas.create_oval(cx-5, cy-5, cx+5, cy+5, fill="red")
+		# for enemy in app.board.enemies:
+		# 	canvas.create_line(enemy.pos[0], enemy.pos[1], app.board.player.pos[0], app.board.player.pos[1], fill="blue")
+		# 	canvas.create_line(enemy.pos[0], enemy.pos[1], enemy.pos[0], app.board.player.pos[1], fill="blue", dash=(5,1))
+		# 	canvas.create_line(enemy.pos[0], app.board.player.pos[1], app.board.player.pos[0], app.board.player.pos[1], fill="blue", dash=(5,1))
+		# 	canvas.create_text(enemy.pos[0] + app.enemyImg.width * 2, enemy.pos[1] + app.enemyImg.height / 2,
+		# 					   text=enemy.rot, font="Arial 10 bold", fill="black")
 
 def playGame():
 	runApp(width=res[0], height=res[1])
